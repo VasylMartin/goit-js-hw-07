fa708e04cef5d41ad434dd0eacd7f67232448129
VasylMartin <vasiliymartiniouk@gmail.com>
2022-07-17 18:19:07 +0300

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+function createGallery(array) {
+  return array.reduce(
+    (acc, item) =>
+      acc +
+      `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
+  );
+}
+const result = createGallery(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+gallery.insertAdjacentHTML("beforeend", result);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
